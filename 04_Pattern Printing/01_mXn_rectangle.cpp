1ffb7d3115cb32ec2229dd63edaa37ca14764380
Himalchals4038 <rakhi_mondal@outlook.com>
2025-12-06 18:16:59 +0530

    6 pm 6th Dec 2025

---
+++ 04_Pattern Printing/01_mXn_rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cout<<"Enter no. of rows: ";
+    cin>>m;
+    cout<<"Enter no. of columns: ";
+    cin>>n;
+    
+    return 0;
+}
