1afc0b6073cf67ed2782f75bca9492ea93c037e2
Himalchals4038 <rakhi_mondal@outlook.com>
2026-03-10 23:20:32 +0530

    11 pm 10/03/26

---
+++ 33. Backtracking/11. Knights Tour/brute2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool kTour(vector<vector<int>> &vec, int i, int j, int count){
+    if (count==vec.size()*vec.size()) return true;
+    return false;
+}
+bool knightsTour(vector<vector<int>> &vec){
+    return kTour(vec,0,0,1);
+}
+int main(){
+    vector<vector<int>> vec = {{0,11,16,5,20},{17,4,19,10,15},{12,1,8,21,6},{3,18,23,14,9},{24,13,2,7,22}};
+    (knightsTour(vec)) ? cout<<"Is Possible" : cout<<"Not Possible";
+    return 0;
+}
